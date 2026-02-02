546decaa2fea0ec1db95159ea75046de6eb290d7
Valentin Yuryev <goodfaiter@gmail.com>
2026-02-02 14:35:43 +0000

    mlp device support

--- src/actuator_network/train_mlp.py
+++ src/actuator_network/train_mlp.py
@@ -17,29 +17,34 @@ def main():
     stride = 4  # Stride for history and future steps, note stride 4 means our final freq is 20Hz
     num_hist = 3  # Number of history steps
     prediction = False  # Whether we are doing prediction or estimation
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     input_cols = ["desired_position_rad_data", "measured_position_rad_data", "measured_velocity_rad_per_sec_data"]
     # input_cols = ["delta_position_rad_data", "measured_velocity_rad_per_sec_data"]
-    output_cols = ["calculated_acceleration_meter_per_sec2_data", "load_newton_data"]
+    output_cols = ["load_newton_data"]
+    # output_cols = ["load_newton_data"]
     mcap_file_paths = [
-        ("/workspace/data/training_data/2026_01_20/rosbag2_2026_01_20-18_28_50_0.mcap", 1.26 / 4.81),  # All spring 1 data, spring constant kg/rad
-        ("/workspace/data/training_data/2026_01_21/rosbag2_2026_01_21-12_55_29_0.mcap", 1.26 / 4.81),  # All spring 1 data
-        ("/workspace/data/training_data/2026_01_21/rosbag2_2026_01_21-12_58_39_0.mcap", 1.26 / 4.81),  # All spring 1 data
-        ("/workspace/data/training_data/2026_01_21/rosbag2_2026_01_21-13_00_05_0.mcap", 1.26 / 4.81),  # All spring 1 data
-        # "/workspace/data/training_data/2026_01_21/rosbag2_2026_01_21-13_01_03_0.mcap", # Test with spring
-        # ("/workspace/data/training_data/2026_01_22/rosbag2_2026_01_22-16_06_21_0.mcap", None),  # Blocked data
-        # ("/workspace/data/training_data/2026_01_22/rosbag2_2026_01_22-16_20_19_0.mcap", None),  # Blocked data
-        # "/workspace/data/training_data/2026_01_22/rosbag2_2026_01_22-16_30_52_0.mcap", # Test with blocked
+        ("/workspace/data/training_data/2026_01_28/rosbag2_2026_01_28-13_18_06_0.mcap", None), 
+        ("/workspace/data/training_data/2026_01_28/rosbag2_2026_01_28-13_24_12_0.mcap", None),
+        ("/workspace/data/training_data/2026_01_28/rosbag2_2026_01_28-13_28_01_0.mcap", None),
+        ("/workspace/data/training_data/2026_01_28/rosbag2_2026_01_28-13_32_59_0.mcap", None),
+        ("/workspace/data/training_data/2026_01_28/rosbag2_2026_01_28-13_43_27_0.mcap", None),
+        ("/workspace/data/training_data/2026_01_28/rosbag2_2026_01_28-13_45_05_0.mcap", None),
+        ("/workspace/data/training_data/2026_01_28/rosbag2_2026_01_28-13_46_19_0.mcap", None),
+        # ("/workspace/data/training_data/2026_01_28/rosbag2_2026_01_28-14_00_33_0.mcap", None),
+        ("/workspace/data/training_data/2026_01_28/rosbag2_2026_01_28-14_16_41_0.mcap", None),
+        ("/workspace/data/training_data/2026_01_28/rosbag2_2026_01_28-14_18_00_0.mcap", None),
+        ("/workspace/data/training_data/2026_01_28/rosbag2_2026_01_28-14_19_28_0.mcap", None),
     ]
 
-    all_inputs = torch.empty((0, len(input_cols) * num_hist))
-    all_outputs = torch.empty((0, len(output_cols)))
+    all_inputs = torch.empty((0, len(input_cols) * num_hist), device=device)
+    all_outputs = torch.empty((0, len(output_cols)), device=device)
     for mcap_file_path, spring_constant in mcap_file_paths:
         data_df = read_mcap_to_dataframe(mcap_file_path)
         data_df_extrapolated = extrapolate_dataframe(data_df, freq=freq)
         process_dataframe(data_df_extrapolated, spring_constant=spring_constant)
         data_df_to_mcap(data_df_extrapolated, mcap_file_path.replace(".mcap", "_processed"))
         # data_df_extrapolated.to_csv(mcap_file_path.replace(".mcap", "_processed.csv"))
-        col_names, data_tensor = pandas_to_torch(data_df_extrapolated, device="cpu")
+        col_names, data_tensor = pandas_to_torch(data_df_extrapolated, device=device)
         input_indices = [col_names.index(col) for col in input_cols]
         output_indices = [col_names.index(col) for col in output_cols]
         inputs = process_inputs(data_tensor[:, input_indices], stride=stride, num_hist=num_hist, prediction=prediction)
@@ -49,7 +54,7 @@ def main():
 
     inputs_normalized, inputs_mean, inputs_std = normalize_tensor(all_inputs)
     outputs_normalized, outputs_mean, outputs_std = normalize_tensor(all_outputs)
-    model = TorchMlpModel(input_size=inputs_normalized.shape[-1], output_size=outputs_normalized.shape[-1], hidden_layers=[32, 32])
+    model = TorchMlpModel(input_size=inputs_normalized.shape[-1], output_size=outputs_normalized.shape[-1], hidden_layers=[16, 16], device=device)
     wrapped_model = ScaledModelWrapper(model, inputs_mean, inputs_std, outputs_mean, outputs_std, frequency=freq, history_size=num_hist, stride=stride, prediction=prediction, input_columns=input_cols, output_columns=output_cols)
     model_saver = ModelSaver(wrapped_model, "/workspace/data/output_data/")
     train(model, inputs_normalized, outputs_normalized, model_saver=model_saver)
